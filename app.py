b127ccf9a0a36fd672e42947002050977c4cd7d4
Yuniel Garcia <garciayuni84@gmail.com>
2019-01-21 16:29:33 -0500

    run for prod

--- app.py
+++ app.py
@@ -390,5 +390,6 @@ def catalog_item_json(item_id):
 
 if __name__ == '__main__':
     app.secret_key = 'adjsfkls$%@#$REWGDFgfdg@$45432%@$%35asdfdsafas'
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    # app.debug = True
+    app.run()
+    # app.run(host='0.0.0.0', port=5000)
